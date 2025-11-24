0f206062e95933ab8e8b4d91f7b5d8e17b65b0d1
kyano <nyarurato202@gmail.com>
2025-11-24 12:03:28 +0900

    [feat]: add NURBS curve join

--- src/Operation/JoinOperator.cs
+++ src/Operation/JoinOperator.cs
@@ -15,6 +15,116 @@ namespace NurbsSharp.Operation
     /// </summary>
     public class JoinOperator
     {
-        //TODO: Join NURBS Curves
+        /// <summary>
+        /// (en) Join two NURBS curves with C0 (positional) continuity.
+        /// The end point of curve1 must match the start point of curve2.
+        /// NOTE: This method does NOT guarantee C1 (tangent) continuity.
+        /// (ja) 2つのNURBS曲線をC0（位置）連続性で結合します。
+        /// curve1の終点はcurve2の始点と一致する必要があります。
+        /// 注意: このメソッドはC1（接線）連続性を保証しません。
+        /// </summary>
+        /// <param name="curve1">First curve</param>
+        /// <param name="curve2">Second curve</param>
+        /// <param name="tolerance">Tolerance for end point matching and weight comparison</param>
+        /// <returns>Joined NURBS curve</returns>
+        public static NurbsCurve JoinCurves(NurbsCurve curve1, NurbsCurve curve2, double tolerance = 1e-6)
+        {
+
+            // Join two NURBS curves. Join at the end of curve1 and the start of curve2.
+            Guard.ThrowIfNull(curve1, nameof(curve1));
+            Guard.ThrowIfNull(curve2, nameof(curve2));
+
+            NurbsCurve first_curve = curve1;
+            NurbsCurve second_curve = curve2;
+
+            // Check G0 continuity (position)
+            var knots1 = first_curve.KnotVector.Knots;
+            var knots2 = second_curve.KnotVector.Knots;
+            Vector3Double endPos1 = first_curve.GetPos(knots1[knots1.Length - 1]); //estimate clamped knot
+            Vector3Double startPos2 = second_curve.GetPos(knots2[0]);
+            if (endPos1.DistanceTo(startPos2) > tolerance)
+            {
+                throw new InvalidOperationException("The end point of curve1 and the start point of curve2 are not coincident.");
+            }
+
+            // Check weight continuity at join point
+            var cp1 = first_curve.ControlPoints;
+            var cp2 = second_curve.ControlPoints;
+            if (Math.Abs(cp1[cp1.Length - 1].Weight - cp2[0].Weight) > tolerance)
+            {
+                throw new InvalidOperationException("The weights at the join point do not match.");
+            }
+
+            int degree1 = first_curve.Degree;
+            int degree2 = second_curve.Degree;
+
+            if (degree1 != degree2)
+            {
+                // degree elevation needed
+                var targetDegree = Math.Max(degree1, degree2);
+                if (degree1 < targetDegree)
+                {
+                    first_curve = DegreeOperator.ElevateDegree(first_curve, targetDegree - degree1);
+                    cp1 = first_curve.ControlPoints; // Update reference after elevation
+                    knots1 = first_curve.KnotVector.Knots;
+                }
+                else if (degree2 < targetDegree)
+                {
+                    second_curve = DegreeOperator.ElevateDegree(second_curve, targetDegree - degree2);
+                    cp2 = second_curve.ControlPoints; // Update reference after elevation
+                    knots2 = second_curve.KnotVector.Knots;
+                }
+            }
+            // Now both curves have the same degree
+            int degree = first_curve.Degree;
+
+            // Merge Control Points
+            // curve1: 0 ... n1
+            // curve2: 0 ... n2
+            // result: 0 ... n1, 1 ... n2 (skip curve2[0] because curve2[0] == curve1[n1])
+            var newControlPoints = new ControlPoint[cp1.Length + cp2.Length - 1];
+            Array.Copy(cp1, 0, newControlPoints, 0, cp1.Length);
+            Array.Copy(cp2, 1, newControlPoints, cp1.Length, cp2.Length - 1);
+
+            // Merge Knot Vectors
+            // curve1 knots: u_0 ... u_m1
+            // curve2 knots: v_0 ... v_m2
+            // result: u_0 ... u_{m1-1}, (v_{p+1} + shift) ... (v_{m2} + shift)
+            // where shift = u_{m1} - v_{0} (assuming standard clamped, v_0 = v_p)
+            // For clamped knot vectors, max_u and min_v are at the ends
+            
+            double max_u = knots1[knots1.Length - 1]; //estimate clamped knot
+            double min_v = knots2[0];
+            double shift = max_u - min_v;
+                        
+            // The knot at u_max should have multiplicity p to ensure C0 continuity.
+            // In CLAMPED knot vector, u_max has multiplicity p+1.
+            // take all knots of curve1 except the last one
+            // curve1 (p=2): 0,0,0, 1,1,1 (3 CPs)
+            // curve2 (p=2): 0,0,0, 1,1,1 (3 CPs)
+            // knots1: 0,0,0,1,1,1. Remove last -> 0,0,0,1,1
+            // knots2: 0,0,0,1,1,1. Remove first p+1 (3) -> 1,1,1. Shift by (1-0)=1 -> 2,2,2
+            // Result: 0,0,0,1,1, 2,2,2. Length = 8.
+
+            var newKnotsList = new List<double>();
+            
+            // Add all knots from curve1 except the last one
+            for (int i = 0; i < knots1.Length - 1; i++)
+            {
+                newKnotsList.Add(knots1[i]);
+            }
+
+            // Add knots from curve2 starting from p+1, shifted
+            for (int i = degree + 1; i < knots2.Length; i++)
+            {
+                newKnotsList.Add(knots2[i] + shift);
+            }
+
+            var newKnotVector = new KnotVector(newKnotsList.ToArray(), degree);
+
+            return new NurbsCurve(degree, newKnotVector, newControlPoints);
+        }
+
+        // TODO: JoinSurfaces method can be implemented similarly, handling U and V directions.
     }
 }
---
+++ src/UnitTests/Operation/JoinOperatorTest.cs
@@ -0,0 +1,339 @@
+using NUnit.Framework;
+using NurbsSharp.Core;
+using NurbsSharp.Geometry;
+using NurbsSharp.IO.IGES;
+using NurbsSharp.Operation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace UnitTests.Operation
+{
+    [TestFixture]
+    public class JoinOperatorTest
+    {
+        [Test]
+        public void JoinLinearCurves()
+        {
+            // Curve 1: (0,0,0) -> (1,0,0)
+            var cp1 = new ControlPoint[]
+            {
+                new ControlPoint(0, 0, 0),
+                new ControlPoint(1, 0, 0)
+            };
+            var kv1 = KnotVector.GetClampedKnot(1, 2); // {0,0,1,1}
+            var c1 = new NurbsCurve(1, kv1, cp1);
+
+            // Curve 2: (1,0,0) -> (2,0,0)
+            var cp2 = new ControlPoint[]
+            {
+                new ControlPoint(1, 0, 0),
+                new ControlPoint(2, 0, 0)
+            };
+            var kv2 = KnotVector.GetClampedKnot(1, 2); // {0,0,1,1}
+            var c2 = new NurbsCurve(1, kv2, cp2);
+
+            var joined = JoinOperator.JoinCurves(c1, c2);
+
+            using (Assert.EnterMultipleScope())
+            {
+                Assert.That(joined.Degree, Is.EqualTo(1));
+                Assert.That(joined.ControlPoints.Length, Is.EqualTo(3));
+                Assert.That(joined.ControlPoints[0].Position.X, Is.EqualTo(0));
+                Assert.That(joined.ControlPoints[1].Position.X, Is.EqualTo(1));
+                Assert.That(joined.ControlPoints[2].Position.X, Is.EqualTo(2));
+            }
+
+            // Expected knots: 0,0, 1, 2,2
+            using (Assert.EnterMultipleScope())
+            {
+                Assert.That(joined.KnotVector.Knots.Length, Is.EqualTo(5));
+                Assert.That(joined.KnotVector.Knots[0], Is.EqualTo(0));
+                Assert.That(joined.KnotVector.Knots[1], Is.EqualTo(0));
+                Assert.That(joined.KnotVector.Knots[2], Is.EqualTo(1));
+                Assert.That(joined.KnotVector.Knots[3], Is.EqualTo(2)); // shifted by 1
+                Assert.That(joined.KnotVector.Knots[4], Is.EqualTo(2));
+            }
+
+            Assert.That(joined.GetLength(), Is.EqualTo(2.0).Within(1e-6));
+
+            double[] sampler_point = [0.0,0.001, 0.5, 0.99,1.0,1.01,1.5, 1.999,2.0];
+            foreach (var u in sampler_point)
+            {
+                Vector3Double estimated_u;
+                if(u <= 1.0)
+                {
+                    estimated_u = c1.GetPos(u); // First curve
+                }
+                else
+                {
+                    estimated_u = c2.GetPos(u - 1.0); // Second curve
+                }
+                Vector3Double joined_u = joined.GetPos(u);
+                Assert.That(joined_u,Is.EqualTo(estimated_u));
+            }
+
+        }
+
+        [Test]
+        public void JoinQuadraticCurves()
+        {
+            // Curve 1: (0,0,0) -> (1,1,0) -> (2,0,0)
+            var cp1 = new ControlPoint[]
+            {
+                new ControlPoint(0, 0, 0),
+                new ControlPoint(1, 1, 0),
+                new ControlPoint(2, 0, 0)
+            };
+            var kv1 = KnotVector.GetClampedKnot(2, 3); // {0,0,0,1,1,1}
+            var c1 = new NurbsCurve(2, kv1, cp1);
+
+            // Curve 2: (2,0,0) -> (3,-1,0) -> (4,0,0)
+            var cp2 = new ControlPoint[]
+            {
+                new ControlPoint(2, 0, 0),
+                new ControlPoint(3, -1, 0),
+                new ControlPoint(4, 0, 0)
+            };
+            var kv2 = KnotVector.GetClampedKnot(2, 3); // {0,0,0,1,1,1}
+            var c2 = new NurbsCurve(2, kv2, cp2);
+
+            var joined = JoinOperator.JoinCurves(c1, c2);
+
+            using (Assert.EnterMultipleScope())
+            {
+                Assert.That(joined.Degree, Is.EqualTo(2));
+                // 3 + 3 - 1 = 5 CPs
+                Assert.That(joined.ControlPoints.Length, Is.EqualTo(5));
+            }
+
+            // Expected knots: 0,0,0, 1,1, 2,2,2
+            // Length: 5 + 2 + 1 = 8
+            using (Assert.EnterMultipleScope())
+            {
+                Assert.That(joined.KnotVector.Knots.Length, Is.EqualTo(8));
+                Assert.That(joined.KnotVector.Knots[3], Is.EqualTo(1));
+                Assert.That(joined.KnotVector.Knots[4], Is.EqualTo(1));
+                Assert.That(joined.KnotVector.Knots[5], Is.EqualTo(2));
+            }
+
+            double[] sampler_point = [0.0, 0.001, 0.5, 0.99, 1.0, 1.01, 1.5, 1.999, 2.0];
+            foreach (var u in sampler_point)
+            {
+                Vector3Double estimated_u;
+                if (u <= 1.0)
+                {
+                    estimated_u = c1.GetPos(u); // First curve
+                }
+                else
+                {
+                    estimated_u = c2.GetPos(u - 1.0); // Second curve
+                }
+                Vector3Double joined_u = joined.GetPos(u);
+                Assert.That(joined_u, Is.EqualTo(estimated_u));
+            }
+        }
+
+        [Test]
+        public void JoinDifferentDegrees()
+        {
+            // Curve 1: Degree 1
+            var cp1 = new ControlPoint[]
+            {
+                new ControlPoint(0, 0, 0),
+                new ControlPoint(1, 0, 0)
+            };
+            var kv1 = KnotVector.GetClampedKnot(1, 2);
+            var c1 = new NurbsCurve(1, kv1, cp1);
+
+            // Curve 2: Degree 2
+            var cp2 = new ControlPoint[]
+            {
+                new ControlPoint(1, 0, 0),
+                new ControlPoint(2, 1, 0),
+                new ControlPoint(3, 0, 0)
+            };
+            var kv2 = KnotVector.GetClampedKnot(2, 3);
+            var c2 = new NurbsCurve(2, kv2, cp2);
+
+            var joined = JoinOperator.JoinCurves(c1, c2);
+
+            using (Assert.EnterMultipleScope())
+            {
+                // Should be elevated to degree 2
+                Assert.That(joined.Degree, Is.EqualTo(2));
+
+                // Curve 1 elevated to degree 2 will have 3 CPs (0,0,0), (0.5,0,0), (1,0,0) Or similar.
+                // Degree elevation of linear curve (0,0)->(1,0) to degree 2:
+                // New CPs: (0,0), (0.5,0), (1,0)
+                // Knots: 0,0,0, 1,1,1
+
+                // Then join with Curve 2 (3 CPs)
+                // Total CPs: 3 + 3 - 1 = 5
+                Assert.That(joined.ControlPoints.Length, Is.EqualTo(5));
+            }
+
+            double[] sampler_point = [0.0, 0.001, 0.5, 0.99, 1.0, 1.01, 1.5, 1.999, 2.0];
+            foreach (var u in sampler_point)
+            {
+                Vector3Double estimated_u;
+                if (u <= 1.0)
+                {
+                    estimated_u = c1.GetPos(u); // First curve
+                }
+                else
+                {
+                    estimated_u = c2.GetPos(u - 1.0); // Second curve
+                }
+                Vector3Double joined_u = joined.GetPos(u);
+                Assert.That(joined_u, Is.EqualTo(estimated_u));
+            }
+        }
+
+        [Test]
+        public void JoinDisjointCurves_ThrowsException()
+        {
+            var cp1 = new ControlPoint[] { new ControlPoint(0,0,0), new ControlPoint(1,0,0) };
+            var c1 = new NurbsCurve(1, KnotVector.GetClampedKnot(1,2), cp1);
+
+            var cp2 = new ControlPoint[] { new ControlPoint(2,0,0), new ControlPoint(3,0,0) };
+            var c2 = new NurbsCurve(1, KnotVector.GetClampedKnot(1,2), cp2);
+
+            Assert.Throws<InvalidOperationException>(() => JoinOperator.JoinCurves(c1, c2));
+        }
+
+        [Test]
+        public void JoinCurves_DifferentWeights_ThrowsException()
+        {
+            // Curve 1: End point at (1,0,0) with weight 1.0
+            var cp1 = new ControlPoint[]
+            {
+                new ControlPoint(0, 0, 0, 1.0),
+                new ControlPoint(1, 0, 0, 1.0)
+            };
+            var kv1 = KnotVector.GetClampedKnot(1, 2);
+            var c1 = new NurbsCurve(1, kv1, cp1);
+
+            // Curve 2: Start point at (1,0,0) but with weight 2.0
+            var cp2 = new ControlPoint[]
+            {
+                new ControlPoint(1, 0, 0, 2.0), // Different weight!
+                new ControlPoint(2, 0, 0, 1.0)
+            };
+            var kv2 = KnotVector.GetClampedKnot(1, 2);
+            var c2 = new NurbsCurve(1, kv2, cp2);
+
+            // Should throw because weights don't match
+            var ex = Assert.Throws<InvalidOperationException>(() => JoinOperator.JoinCurves(c1, c2));
+            Assert.That(ex.Message, Does.Contain("weight"));
+        }
+
+        [Test]
+        public void JoinCurves_CustomTolerance()
+        {
+            // Curve 1: End point at (1,0,0)
+            var cp1 = new ControlPoint[]
+            {
+                new ControlPoint(0, 0, 0),
+                new ControlPoint(1, 0, 0)
+            };
+            var kv1 = KnotVector.GetClampedKnot(1, 2);
+            var c1 = new NurbsCurve(1, kv1, cp1);
+
+            // Curve 2: Start point at (1.0001,0,0) - slightly off
+            var cp2 = new ControlPoint[]
+            {
+                new ControlPoint(1.0001, 0, 0),
+                new ControlPoint(2, 0, 0)
+            };
+            var kv2 = KnotVector.GetClampedKnot(1, 2);
+            var c2 = new NurbsCurve(1, kv2, cp2);
+
+            // Should fail with default tolerance
+            Assert.Throws<InvalidOperationException>(() => JoinOperator.JoinCurves(c1, c2));
+
+            // Should succeed with larger tolerance
+            var joined = JoinOperator.JoinCurves(c1, c2, tolerance: 1e-3);
+            Assert.That(joined.ControlPoints.Length, Is.EqualTo(3));
+
+            double[] sampler_point = [0.0, 0.001, 0.5, 0.99, 1.0, 1.01, 1.5, 1.999, 2.0];
+            foreach (var u in sampler_point)
+            {
+                Vector3Double estimated_u;
+                if (u <= 1.0)
+                {
+                    estimated_u = c1.GetPos(u); // First curve
+                }
+                else
+                {
+                    estimated_u = c2.GetPos(u - 1.0); // Second curve
+                }
+                Vector3Double joined_u = joined.GetPos(u);
+                using (Assert.EnterMultipleScope())
+                {
+                    Assert.That(joined_u.X, Is.EqualTo(estimated_u.X).Within(1e-3));
+                    Assert.That(joined_u.Y, Is.EqualTo(estimated_u.Y).Within(1e-3));
+                    Assert.That(joined_u.Z, Is.EqualTo(estimated_u.Z).Within(1e-3));
+                }
+            }
+        }
+
+        [Test]
+        public void JoinCurvesDegree4withDiffrentDegree()
+        {
+            var cp1 = new ControlPoint[]
+            {
+                new ControlPoint(0, 0, 1),
+                new ControlPoint(1, 2, 2),
+                new ControlPoint(2, 0, 3),
+                new ControlPoint(3, -2, 4),
+                new ControlPoint(4, 0, 5)
+            };
+
+            var kv1 = KnotVector.GetClampedKnot(4, 5); // Degree 4, 5 CPs
+            var c1 = new NurbsCurve(4, kv1, cp1);
+
+            var cp2 = new ControlPoint[]
+            {
+                new ControlPoint(4, 0, 5),
+                new ControlPoint(-5, -5, -5)
+            };
+            var kv2 = KnotVector.GetClampedKnot(1, 2); // Degree 1, 2 CPs
+            var c2 = new NurbsCurve(1, kv2, cp2);
+
+            var joined = JoinOperator.JoinCurves(c1, c2);
+
+            using (Assert.EnterMultipleScope())
+            {
+                Assert.That(joined.Degree, Is.EqualTo(4));
+                Assert.That(joined.ControlPoints.Length, Is.EqualTo(9)); // 5 + 2 + 3(elevation) - 1 = 9
+            }
+
+            double[] sampler_point = [0.0, 0.001, 0.5, 0.99, 1.0, 1.01, 1.5, 1.999, 2.0];
+            foreach (var u in sampler_point)
+            {
+                Vector3Double estimated_u;
+                if (u <= 1.0)
+                {
+                    estimated_u = c1.GetPos(u); // First curve
+                }
+                else
+                {
+                    estimated_u = c2.GetPos(u - 1.0); // Second curve
+                }
+                Vector3Double joined_u = joined.GetPos(u);
+                Assert.That(joined_u, Is.EqualTo(estimated_u));
+            }
+            
+
+        }
+
+        void TestOutIGES(List<NurbsCurve> geometries, string filePath="testcurve.igs")
+        {
+            using var stream = new FileStream(filePath, FileMode.Create, FileAccess.Write);
+
+            var success = IGESExporter.ExportAsync(geometries, stream);
+        }
+    }
+}
